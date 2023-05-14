5d2c9c27204d5e3c19df5630010691c6024ec4bb
Ismael-Njihia <engishmaelnjihia@gmail.com>
2023-05-14 14:45:27 +0300

    Finished on the Ui? Fetching from the Product.js Array

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
+
+
 
 const App = () => {
   return (
@@ -10,7 +12,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import {Navbar, Nav, Container} from 'react-bootstrap';
 import {FaShoppingCart, FaUser} from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
 import logo from '../assets/logo.png'
 
 const Header = () => {
@@ -8,14 +9,16 @@ const Header = () => {
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
 
             <Container>
-                <Navbar.Brand href="/">
+                <LinkContainer to='/'>
+                <Navbar.Brand>
                     <img src={logo} alt='proshop'/>
                     ProShop</Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className='ms-auto'>
-                        <Nav.Link href="/cart"><FaShoppingCart/> Cart</Nav.Link>
-                        <Nav.Link href="/login"><FaUser/> Sign In</Nav.Link>
+                       <LinkContainer to='/sign in'><Nav.Link ><FaShoppingCart/> Cart</Nav.Link></LinkContainer>
+                       <LinkContainer to='/login'><Nav.Link><FaUser/> Sign In</Nav.Link></LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
 
