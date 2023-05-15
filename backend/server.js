5f1e5f84830a7e46dd4a5bad427fdc4ea9c76cd4
Ismael-Njihia <engishmaelnjihia@gmail.com>
2023-05-15 13:25:48 +0300

    connection to MOngodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 import product from './data/products.js';
 import products from './data/products.js';
-const port = 5000;
+
+const port = process.env.PORT || 8000;
 
 const app = express();
 
