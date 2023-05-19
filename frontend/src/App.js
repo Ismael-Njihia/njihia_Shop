6b25f1d00b61fb969e35ca5f6d78683b50f0bf79
Ismael-Njihia <engishmaelnjihia@gmail.com>
2023-05-19 13:12:51 +0300

    Login functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -16,6 +18,7 @@ const App = () => {
         </Container>
       </main>
       <Footer />
+      <ToastContainer />
     </>
   );
 }
