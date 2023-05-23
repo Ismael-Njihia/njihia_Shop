f9c3b24950924141331ca6c5da5e994156c29898
Ismael-Njihia <engishmaelnjihia@gmail.com>
2023-05-23 18:30:06 +0300

    Finalized on the order Page'

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -1,5 +1,5 @@
 import {Link, useParams} from 'react-router-dom'
-import {Row, Col, ListGroup, Image, Form, Button, Card, } from 'react-bootstrap'
+import {Row, Col, ListGroup, Image, Form, Button, Card, ListGroupItem, } from 'react-bootstrap'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { useGetOrderDetailsQuery } from '../slices/ordersApiSlice'
@@ -27,10 +27,111 @@ const OrderScreen = () => {
           <ListGroup variant='flush'>
             <ListGroup.Item>
               <h2>Shipping</h2>
+              <p>
+                <strong>Name: </strong>
+                {order.user.name}
+              </p>
+
+              <p>
+                <strong>Email: </strong>
+                {order.user.email}
+              </p>
+              <strong>Adress: </strong>
+             P.O BOX {order.shippingAddress.address}-{order.shippingAddress.city}, {order.shippingAddress.country}
+             
+              <p>
+                {order.isDelivered ? (
+                    <Message variant='success'>Delivered on {order.deliveredAt}</Message>
+                ):(
+                    <Message variant='danger'>Not Delivered</Message>
+                )
+                }
+
+              </p>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <h2>Payment Method</h2>
+              
+                <p>
+                  <strong>Method: </strong>
+                  {order.paymentMethod}
+                </p>
+
+                <p>
+                {order.isPaid ? (
+                    <Message variant='success'>Paid on {order.PaidAt}</Message>
+                ):(
+                    <Message variant='danger'>Not Paid</Message>
+                )
+                }
+
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h2>Order Items</h2>
+
+              {order.orderItems.length === 0 ? (
+                <Message variant='danger'>Order is empty</Message>
+              ):(
+                order.orderItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                        <Row>
+                            <Col md={1}>
+                                <Image src={item.image} alt={item.name} fluid rounded/>
+                            </Col>
+                            <Col>
+                                <Link to={`/product/${item.product}`}>{item.name}</Link>
+                            </Col>
+                            <Col md={4}> 
+                            {item.qty} x ${item.price} = ${item.qty * item.price}
+                            
+                            </Col>
+
+                        </Row>
+                    </ListGroup.Item>
+                
+                ))
+              )}
+
             </ListGroup.Item>
           </ListGroup>
         </Col>
-        <Col md={4}></Col>
+        <Col md={4}>
+            <Card>
+                <ListGroup variant='flush'>
+                    <ListGroup.Item>
+                       <h2> Order Summary</h2>
+                      </ListGroup.Item>
+
+                      <ListGroup.Item>
+                          <Row>
+                              <Col>Items</Col>
+                              <Col>${order.itemsPrice}</Col>
+                            </Row> 
+
+                            <Row>
+                              <Col>Shipping Price</Col>
+                              <Col>${order.shippingPrice}</Col>
+                            </Row>
+
+                            <Row>
+                              <Col>Tax</Col>
+                              <Col>${order.taxPrice}</Col>
+                            </Row> 
+
+                            <Row>
+                              <Col>Total</Col>
+                              <Col>${order.totalPrice}</Col>
+                            </Row>  
+                      </ListGroup.Item>
+
+                      {/*Pay order*/}
+                  </ListGroup>
+            </Card>
+
+
+        </Col>
       </Row>
     </>
   );
