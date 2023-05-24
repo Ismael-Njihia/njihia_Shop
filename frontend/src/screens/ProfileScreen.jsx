7d57f983e7b2d9fd63c9b7d9bcfeba7f6930e5c4
Ismael-Njihia <engishmaelnjihia@gmail.com>
2023-05-24 14:59:05 +0300

    Worked on Profile page, Retrieved user Profile Info

---
+++ frontend/src/screens/ProfileScreen.jsx
@@ -0,0 +1,112 @@
+import {useEffect, useState} from 'react';
+import {Table, Form, Button, Row, Col} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap';
+import {useDispatch, useSelector} from 'react-redux';
+import { toast } from 'react-toastify';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { useProfileMutation } from '../slices/usersApiSlice';
+import { setCredentials } from '../slices/authSlice';
+
+const ProfileScreen = () => {
+    const [email, setEmail] = useState('')
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
+    const dispatch = useDispatch()
+
+    const {userInfo} = useSelector(state => state.auth)
+
+
+     const [updateProfile, {isLoading: loadingUpdateProfile}] = useProfileMutation({})
+
+    useEffect(() => {
+        if(userInfo) {
+            setEmail(userInfo.email)
+            setName(userInfo.name)
+        }
+
+    }, [userInfo,userInfo.email, userInfo.name]);
+
+    const submitHandler = async (e)=>{
+        e.preventDefault();
+       if(password !== confirmPassword) {
+           toast.error('Passwords do not match')
+           return
+       }else{
+        try{
+            const res = updateProfile({_id: userInfo._id, email, name, password}).unwrap();
+            
+            dispatch(setCredentials(res))
+            toast.success('Profile Updated')
+
+
+        }catch(err){
+            toast.error(err.message)
+        }
+       }
+
+    }
+
+  return (
+    <Row>
+        <Col md={3}>
+            <h2>User Profile</h2>
+            <Form onSubmit={submitHandler}>
+                <Form.Group controlId='name' className='my-2'>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        type='name'
+                        placeholder='Enter name'
+                        value={name}
+                        onChange={(e)=>setName(e.target.value)}
+                    ></Form.Control>
+                    </Form.Group>
+
+
+                    <Form.Group controlId='email' className='my-2'>
+                    <Form.Label>Email Adress</Form.Label>
+                    <Form.Control
+                        type='email'
+                        placeholder='Enter Email'
+                        value={email}
+                        onChange={(e)=>setEmail(e.target.value)}
+                    ></Form.Control>
+                    </Form.Group>
+
+                    <Form.Group controlId='password' className='my-2'>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Enter password'
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value)}
+                    ></Form.Control>
+                    </Form.Group>
+
+
+                    <Form.Group controlId='confirmPassword' className='my-2'>
+                    <Form.Label></Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Confirm password'
+                        value={confirmPassword}
+                        onChange={(e)=>setConfirmPassword(e.target.value)}
+                    ></Form.Control>
+                    </Form.Group>
+
+                    <Button type='submit' variant='primary' className='my-2'>
+                        Update
+                    </Button>
+                    {loadingUpdateProfile && <Loader />}
+                </Form>
+        </Col>
+        <Col md={9}>
+            <h2>My Orders</h2>
+        </Col>
+    </Row>
+  )
+}
+
+export default ProfileScreen
--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -22,10 +22,17 @@ export const usersApiSlice = apiSlice.injectEndpoints({
          logout: builder.mutation({
             query: `${USERS_URL}/logout}`,
             method: 'POST',
+        }),
+        profile: builder.mutation({
+            query: (data)=>({
+                url: `${USERS_URL}/profile`,
+                method: 'PUT',
+                body: data,
+            })
         })
     })
 
 })
 
 
-export const {useLoginMutation, useLogoutMutation, useRegisterMutation} = usersApiSlice;
+export const {useLoginMutation, useLogoutMutation, useRegisterMutation, useProfileMutation} = usersApiSlice;
